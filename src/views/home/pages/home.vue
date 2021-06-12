df39242845498695e83a0fdfd4f425d8b67c86a0
Силин Дмитрий Сергеевич <dssilin@mts.ru>
2021-06-12 17:26:23 +0300

    rename HelloWorld

--- src/views/home/pages/home.vue
+++ src/views/home/pages/home.vue
@@ -7,7 +7,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/hello-world.vue";
 
 export default defineComponent({
   name: "Home",
